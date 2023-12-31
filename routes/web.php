dd6c059ef1bd2430968692c49a61184f79ef0407
mphstar <bintang.simdig20@gmail.com>
2023-12-31 11:44:07 +0700

    add: layout

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('pages.student.profile');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
